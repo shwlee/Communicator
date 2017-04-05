4d1036b0cc35e7824da17256f8941c9f65fd0173
vactorman <shlee@n3n.co.kr>
2017-04-05 11:40:47 +0900

    - implement client send test

--- ServiceCommunicator/Client/Program.cs
+++ ServiceCommunicator/Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Communication;
 using Define.Classes.Args;
@@ -14,30 +15,37 @@ namespace Client
         static void Main(string[] args)
         {
             Console.WriteLine("Ready to Start!");
-            
+
             var communicator = new Communicator();
             communicator.ConnectToService("172.16.20.70", ServicePort);
 
             Console.WriteLine("Connect to Service Start! IP : {0}, Port : {1}", ServiceIp, ServicePort);
-            
+
             Console.ReadLine();
 
             SendTest(communicator);
-            
+
             Console.ReadLine();
         }
 
         private static async Task SendTest(Communicator com)
         {
-            var response =
-               await com.Send<IServiceStatus, SetServiceStatusResponse>(
-                    service => service.SetServiceStatus(new SetServiceStatusRequest
-                    {
-                        ClientHash = 20,
-                        Status = true
-                    }));
-
+            var response = await com.Send((IServiceStatus service) => service.SetServiceStatus(new SetServiceStatusRequest
+            {
+                ClientHash = 20,
+                Status = true
+            }));
+            
             Console.WriteLine("Received Response. IsSuccess : {0}", response.IsSuccess);
+        }
+
+        private static async Task<TResult> SendAsync<TInterface, TResult>(Expression<Func<TInterface, TResult>> method, Communicator com)
+            where TInterface : class
+            where TResult : class
+        {
+            var response = await com.Send(method);
+
+            return response;
         }
     }
 }
